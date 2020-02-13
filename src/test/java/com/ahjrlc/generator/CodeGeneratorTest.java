7964b8807a0e60debf3b220f86155dec1da4503e
aachen0 <frank717@qq.com>
2020-02-13 10:36:58 +0800

    调整文件结构

---
+++ src/test/java/com/ahjrlc/generator/CodeGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.ahjrlc.generator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CodeGeneratorTest {
+
+    @Test
+    public void generateTableMvc() {
+    }
+
+    @Test
+    public void generateJsp() {
+    }
+
+    @Test
+    public void generateControllerAndService() {
+    }
+}
---
+++ src/test/java/com/ahjrlc/generator/mybatis/MybatisGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.ahjrlc.generator.mybatis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MybatisGeneratorTest {
+
+    @Test
+    public void generate() {
+    }
+}
