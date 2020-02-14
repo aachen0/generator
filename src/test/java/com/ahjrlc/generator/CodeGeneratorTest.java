5e43c219bca6a5e92bb738cf344763c89fd8f3d6
付向峰 <frank717@qq.com>
2020-02-14 13:21:19 +0800

    测试版本保护

--- src/test/java/com/ahjrlc/generator/CodeGeneratorTest.java
+++ src/test/java/com/ahjrlc/generator/CodeGeneratorTest.java
@@ -1,10 +1,13 @@
 package com.ahjrlc.generator;
 
+import com.ahjrlc.generator.mybatis.MybatisGenerator;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CodeGeneratorTest {
+    CodeGenerator codeGenerator = new CodeGenerator("");
+    MybatisGenerator mybatisGenerator = new MybatisGenerator("");
 
     @Test
     public void generateTableMvc() {
